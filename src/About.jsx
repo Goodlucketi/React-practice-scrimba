d560d7176d9ccdf40f56ffb7ab06f5ea8bb34b04
unknown <goodlucketim94@gmail.com>
2024-07-06 17:19:10 +0100

    update

--- src/About.jsx
+++ src/About.jsx
@@ -5,7 +5,13 @@ export default function About(){
     return(
         <main>
             <h2>About</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum qui nam velit dicta sapiente aut incidunt voluptatibus dignissimos vitae iusto consectetur tenetur, facilis quis at! Nemo deserunt laboriosam quia molestias aliquam consequatur nam. Delectus, enim dignissimos cum quod qui consectetur modi fuga, adipisci cumque, quo minima tenetur error voluptate. Aspernatur!</p>
+            <p>
+                Hello, I'm Mfoniso Fabian Etim, a passionate and dedicated technology enthusiast from Akwa Ibom State, Nigeria. With a Bachelor's degree in Computer Engineering from the University of Uyo, I have cultivated a deep understanding and proficiency in various web technologies including HTML, CSS, JavaScript, Vue.js, React.js, PHP, and Node.js.
+
+                My professional journey has seen me take on roles that hone my skills and broaden my experience in the tech industry. As a Web Instructor at Classic Systems Infotech Limited in Ado Ekiti, I had the opportunity to share my knowledge and inspire budding developers. My role as a Front End Developer at Kinplus Technologies in Ekiti State allowed me to work on innovative projects, pushing the boundaries of web development.
+
+                Driven by a strong belief in the power of technology to transform lives, I founded Meln Hub in Uyo, Akwa Ibom State. Through this initiative, I aim to create a community where technology enthusiasts can collaborate, learn, and innovate together.
+            </p>
 
         </main>
     )
--- src/Interests.jsx
+++ src/Interests.jsx
@@ -5,7 +5,10 @@ export default function Interests(){
     return(
         <main>
            <h2>Interests</h2>
-           <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum, quibusdam, accusantium obcaecati saepe repellendus itaque voluptatum in esse voluptatibus fuga laboriosam ab iste ratione, a molestias vitae ducimus! Veritatis iure itaque quidem, fugit necessitatibus quaerat deleniti esse aut ipsum eos tenetur? Maxime fuga aliquam ut illo fugiat adipisci quod a!</p>
+           <p>
+            Beyond my professional endeavors, I have a deep passion for giving back to humanity. I am committed to solving real-world problems through technology and educating others about the vast possibilities available in this field. My hobbies, which include coding, meditating, playing football, gaming, and making friends, keep me balanced and constantly fuel my creativity.
+            I believe that technology is a tool for change and empowerment. By leveraging my skills and experience, I strive to make a positive impact on society and help others achieve their purpose.!
+           </p>
         </main>
     )
 }
